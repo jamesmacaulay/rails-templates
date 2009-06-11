374f0e438c2d88b3d5b63ec8b041a2ecd7f90600
James MacAulay <james@jadedpixel.com>
2009-06-11 14:33:58 -0400

    ignore shopify.yml; less commits

--- shopify_app.rb
+++ shopify_app.rb
@@ -4,18 +4,17 @@ file '.gitignore', <<GITIGNORE
 .dotest
 *.log
 *.sqlite3
+
+config/shopify.yml
 GITIGNORE
 
 git :init
-git :add => '.', :commit => '-am Rails'
 
 plugin 'shopify_app', :git => 'git://github.com/Shopify/shopify_app'
-
-git :add => '.', :commit => '-am "shopify_app plugin"'
 
 api_key = ask("What is your app's API key?")
 secret = ask("What is your app's shared secret?")
 
 generate 'shopify_app', api_key + ' ' + secret
 
-git :add => '.', :commit => '-am "shopify_app generated"'
+git :add => '.', :commit => '-am "fresh shopify_app generated"'
